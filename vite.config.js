4dee5de9ef304d321fbd46498e99424906799b44
Martin Stewart <martin.stewart.100@strath.ac.uk>
2022-11-02 11:41:49 +0000

    remove base url

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig(({command}) => {
-
-  	return {
-		plugins: [vue()],
-		base: command === 'build' ? '/squashlist/' : '/',
-		build: {
-			outDir: 'docs'
-		}
-  	}
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    outDir: 'docs'
+  }
 })
