745d4b9311c9b56cc9459e88370443b494565309
Martin Stewart <m11t@mm.st>
2023-11-29 10:04:17 +0000

    Update m11t icon

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
 	<footer class="footer">
-        <img src="//m11t.dev/assets/m11t.7479ad49.jpg" alt="m11t" /> a <a href="//www.m11t.dev">m11t</a> project
+        <img src="//m11t.dev/media/img/m11t.jpg" alt="m11t" /> a <a href="//www.m11t.dev">m11t</a> project
 	</footer>
 </template>
