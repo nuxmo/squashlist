6e1df88277a825ac3ad8e6917ab774bfe5dc8035
Martin Stewart <m11t@mm.st>
2024-08-06 08:06:07 +0100

    icon image url

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
 	<footer class="footer">
-        <img src="//m11t.dev/media/img/m11t.jpg" alt="m11t" /> a <a href="/">m11t</a> project
+        <img src="//m11tdev.github.io/media/img/m11t.jpg" alt="m11t" /> a <a href="/">m11t</a> project
 	</footer>
 </template>
