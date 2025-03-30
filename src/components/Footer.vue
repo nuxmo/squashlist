5aacc4696aa89fa2d3798e32f4729cd89bfd20f0
Martin Stewart <m11t@mm.st>
2025-03-30 09:11:13 +0100

    Footer change

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
 	<footer class="footer">
-        <img src="//m11tdev.github.io/media/img/m11t.jpg" alt="m11t" /> a <a href="/">m11t</a> project
+        <img src="//nuxmo.github.io/media/img/m11t.jpg" alt="m11t" /> a <a href="/">Nuxmo</a> project
 	</footer>
 </template>
